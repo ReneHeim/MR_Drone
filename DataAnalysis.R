69bf2005e6ae98ef1811b761cd574e927c8806e8
Rene Heim <rene.heim@hdr.mq.edu.au>
2018-04-30 22:40:23 +0200

    Version 0.02
    Edit:
    - Started to clean code
    - Improved comments
    - Added explanation to sourced fcts
    
    Next:
    - Further cleaning required
    - Test if still running after clean

--- DataAnalysis.R
+++ DataAnalysis.R
@@ -1,51 +1,54 @@
-
-# Introductions ---------------------------------------------------------------------------------------
-
 ####
 # The following code reproduces results of the article:
 # 
-# Heim, Wright and Oldeland, 2018. Using drones and multispectral imagery to detect myrtle rust on a
-# lemon myrtle plantation. JOURNAL
+# Heim, Wright and Oldeland, 2018. Using drones and multispectral imagery to 
+#detect myrtle rust on a lemon myrtle plantation. JOURNAL
 #
 # The code is split in two major sections according to our research questions.
 #
-# A) Can we discriminate fungicide treated and untreated lemon myrtle trees based on multispectral 
-#    imagery captured by a drone?
-#       - Load GIS files to extract raw pixel data for all available spectral bands and pre-defined
-#         classes (TReated, UNtreated, SHAdow)
-#       - Raw pixel data will be classified using a random forest classifier and all spectral bands.
+# A)  Can we discriminate fungicide treated and untreated lemon myrtle trees 
+#     based on multispectral imagery captured by a drone?
+#       - Load GIS files to extract raw pixel data for all available spectral 
+#         bands and pre-defined classes (TReated, UNtreated, SHAdow)
+#       - Raw pixel data will be classified using a random forest classifier and 
+#         all spectral bands.
 #       - Accuracy metrics will be reported in an error matrix
 #
-# B) Is a leaf-scale classification equally accurate as a canopy-scale classification?
-#       - Resample hyperspectral leaf data to multispectral (Micasense) resolution to be able to
-#         compare two classifications based on the same sensor.
+# B)  Is a leaf-scale classification equally accurate as a canopy-scale 
+#     classification?
+#       - Resample hyperspectral leaf data to multispectral (Micasense) 
+#         resolution to be able to compare two classifications based on the same 
+#         sensor.
 #       - Perform classifcation on this resampled data.
-#       - Report accuracies and compare to results found in A) (raw pixel data vs reflectance)
+#       - Report accuracies and compare to results found in A) (DN vs refl.)
 #
-# C) Are the important spectral regions found on the leaf scale similar to those found from the air?
+# C)  Are the important spectral regions found on the leaf scale similar to 
+#     those found from the air?
 #       - Compare relevant spectral regions between A) and B)
 ####
 
 
-# Setting up project ---------------------------------------------------------------------------------
+# Setting up coding environment ------------------------------------------------
 
-dir.create('data', FALSE, FALSE)
+
+dir.create('data', FALSE, FALSE) #creating directories
 dir.create('R', FALSE, FALSE)
 dir.create('output', FALSE, FALSE)
 
-# Installing/loading packages and functions ----------------------------------------------------------
+install.packages(c("rgdal",
+"raster",
+"tictoc",
+"caret",
+"tidyverse",
+"e1071",
+"randomForest",
+"gdata",
+"hsdar",
+"utils",
+"raster",
+"rasterVis")) #installing required packages (pls uncomment to and run)
 
-# install.packages(c("rgdal", 
-# "raster", 
-# "tictoc", 
-# "caret", 
-# "tidyverse", 
-# "e1071", 
-# "randomForest", 
-# "gdata",
-# "hsdar"))
-
-library(rgdal)
+library(rgdal) #load installed pkgs
 library(raster)
 library(tictoc)
 library(tidyverse)
@@ -54,26 +57,22 @@ library(e1071)
 library(randomForest)
 library(gdata)
 library(hsdar)
-require(rgdal)
-require(utils)
-require(raster)
-require(rasterVis)
-#require(dismo)
+library(utils)
+library(raster)
+library(rasterVis)
 
 
-source("R/20171224_FUN_raw2speclibhsdar.R")
-source("R/20170601_FUN_DropCatVar.R")
-source("R/FUN_extract_pixel.R")
+source("R/20171224_FUN_raw2speclibhsdar.R")#coverts spec data to hsdar lib
+source("R/20170601_FUN_DropCatVar.R")#drops factor and factor level
+source("R/FUN_extract_pixel.R")#extracting DN from GTIFF based on a shp file 
 
 
-# SECTION A ----------------------------------------------------------------------------
-
-# Extract raw pixel values from aerial imagery  ----------------------------------------
+# SECTION A --------------------------------------------------------------------
 
 
 # Load multispectral image
 
-img <- brick("data/test.tif") # Change to path that contains .tif file
+img <- brick("data/FullOrtho_crop.tif") # Change to path that contains .tif file
 
 img@data@names <- c("Blue", "Green", "Red", "NIR", "RedEdge", "Alpha")
 plotRGB(img, r = 3, g = 2, b = 1) # test if image is shown correctly, rgb combination arbitrary
@@ -111,7 +110,6 @@ for(i in UN_num){
 dfAll$class[dfAll$class == i] <- "UN"
 }
 
-
 for(i in TR_num){
   dfAll$class[dfAll$class == i] <- "TR"
 }
@@ -129,18 +127,19 @@ dfAll$class <- as.factor(dfAll$class)
 
 names(dfAll)[7] <- c("Type")
 
-dfAll[,1:5] <- dfAll[,1:5]/65535
+dfAll[,1:5] <- dfAll[,1:5]/65535#divide by 65535 to yield refl between 0 and 1
 
 
 # Save and reload table containing raw pixel values and classes
 
 write.csv(dfAll[,1:7], 'output/2018MyrtleRust_Refl.csv', row.names = FALSE)
 classif <- read.csv("output/2018MyrtleRust_Refl.csv")
-classif <- classif[,c(1,2,3,4,5,7)] # remove alpha band
+classif <- classif[,c(1,2,3,4,5,7)] # remove alpha band, not required for clas
 unique(classif$Type)
 
 
-# Random Forest Classification --------------------------------------------
+# Random Forest Classification -------------------------------------------------
+
 
 set.seed(20180427)
 
@@ -157,7 +156,7 @@ rfControl <- trainControl(
   number = 100
   )
 
-# Approximate mtry (Number of variables randomly sampled as candidates at each split)
+# Approximate mtry (# of vars randomly sampled as candidates at each split)
 
 rfGrid <- expand.grid(mtry = seq(1, ncol(train)-1, 1)) 
 
@@ -176,16 +175,18 @@ rfFit #Model output based on training data
 
 # RF Model Testing
 
-rfPred <- predict.train(rfFit, test[, !names(test) %in% c("Type")], type = "raw")
+rfPred <- 
+  predict.train(rfFit, test[, !names(test) %in% c("Type")], type = "raw")
 
 toc() # Stop timing
 
 # Collect results in a list and save
 
-Mica.Prediction <- list(fit = rfFit,
-                    pred = predict.train(rfFit, test[, !names(test) %in% c("Type")], type = "raw"),
-                    confusion = confusionMatrix(rfPred, test$Type),
-                    varImp = varImp(rfFit, scale = FALSE))
+Mica.Prediction <- 
+  list(fit = rfFit,
+  pred = predict.train(rfFit, test[, !names(test) %in% c("Type")], type = "raw"),
+  confusion = confusionMatrix(rfPred, test$Type),
+  varImp = varImp(rfFit, scale = FALSE))
 
 sink(file = 'output/I_Mica3ClassPred.txt')
 Mica.Prediction
@@ -195,28 +196,35 @@ saveRDS(Mica.Prediction, 'output/I_Mica3ClassPred.rds')
 Mica.Prediction <- readRDS("output/I_Mica3ClassPred.rds")
 
 
-# Create RiskMap ----------------------------------------------------------
+# Create RiskMap ---------------------------------------------------------------
 
 
-imgpred <-  brick("data/20180427_orthophoto_noground_MRDrone.tif") # load lemon myrtle trees without ground to predict wo grass
-    riskpre <- subset(imgpred, 1:5) # remove alpha/transparency channel as it was not used as a predictor var in rf 
+imgpred <-  brick("data/20180427_orthophoto_noground_MRDrone.tif") # load lemon 
+#myrtle trees without ground to predict wo grass
+    riskpre <- subset(imgpred, 1:5) # remove alpha/transparency channel as it 
+    #was not used as a predictor var in rf 
         risk <- riskpre/65535 # divide by 65535 to change values to reflectance
-            risk@data@names <- c("Blue", "Green", "Red", "NIR", "RedEdge", "Alpha") # rename bands to predict
+            risk@data@names <- 
+              c("Blue", "Green", "Red", "NIR", "RedEdge", "Alpha") # rename
 
-riskpred <- predict(risk, Mica.Prediction$fit)
-    plot(riskpred)
+            riskpred <- predict(risk, Mica.Prediction$fit)
+                plot(riskpred)
 
-writeRaster(test, "output/test.tif", format = "GTiff")
-
-
-# Visualize RiskMap -------------------------------------------------------
+                currentDate <- Sys.Date()
+                rstFileName <- paste("output/riskmap",currentDate,".tif",sep="")
+                writeRaster(riskpred, file=rstFileName, format = "GTiff")
+                #NOTE should wrap this file output in function
+                
+# Visualize RiskMap ------------------------------------------------------------
 
 plotRGB(img, r = 3, g = 2, b = 1)
-plot(riskpred, add = T, legend = F, col = rev(rainbow(10, alpha = 0.35)))
+plot(riskpred, add = T, legend = F)
 
-# SECTION B ----------------------------------------------------------------------------
 
-# Resample hyperspec to multispec ------------------------------------------------------
+# SECTION B --------------------------------------------------------------------
+
+
+# Resample hyperspec to multispec ----------------------------------------------
 
 
 # Load hyperspectral leaf data
@@ -251,7 +259,7 @@ newnamesM <- c("Type", "Blue", "Green", "Red", "RedEdge", "NIR")
 
 names(micadata) <- newnamesM
 
-# Random Forest Classification (MultispecSimulate) ---------------------------------------------
+# Random Forest Classification (MultispecSimulate) -----------------------------
 
 # Partition Data
 
@@ -266,7 +274,7 @@ rfControl <- trainControl(
   number = 100
 )
 
-# Approximate mtry (Number of variables randomly sampled as candidates at each split)
+# Approximate mtry (# of vars randomly sampled as candidates at each split)
 
 rfGrid <- expand.grid(mtry = seq(1, ncol(trainM)-1, 1)) 
 
@@ -285,16 +293,18 @@ rfFit.M #Model output based on training data
 
 # RF Model Testing
 
-rfPred.M <- predict.train(rfFit.M, testM[, !names(testM) %in% c("Type")], type = "raw")
+rfPred.M <- 
+  predict.train(rfFit.M, testM[, !names(testM) %in% c("Type")], type = "raw")
 
 toc() # Stop timing
 
 # Collect results in a list and save
 
-Mica.Resamp.Prediction <- list(fit = rfFit.M,
-                        pred = predict.train(rfFit.M, testM[, !names(testM) %in% c("Type")], type = "raw"),
-                        confusion = confusionMatrix(rfPred.M, testM$Type),
-                        varImp = varImp(rfFit.M, scale = FALSE))
+Mica.Resamp.Prediction <- 
+  list(fit = rfFit.M,
+  pred = predict.train(rfFit.M, testM[, !names(testM) %in% c("Type")], type = "raw"),
+  confusion = confusionMatrix(rfPred.M, testM$Type),
+  varImp = varImp(rfFit.M, scale = FALSE))
 
 sink(file = 'output/II_ResampLeafPred.txt')
 Mica.Resamp.Prediction
@@ -303,7 +313,7 @@ sink()
 saveRDS(Mica.Resamp.Prediction, 'output/II_ResampLeafPred.rds')
 
 
-# Random Forest Classification (MultispecDrone) ---------------------------
+# Random Forest Classification (MultispecDrone) --------------------------------
 
 #Create data (drop shadows)
 
@@ -324,7 +334,7 @@ rfControl <- trainControl(
   number = 100
 )
 
-# Approximate mtry (Number of variables randomly sampled as candidates at each split)
+# Approximate mtry (# of vars randomly sampled as candidates at each split)
 
 rfGrid <- expand.grid(mtry = seq(1, ncol(trainD)-1, 1)) 
 
@@ -342,16 +352,18 @@ rfFit.M #Model output based on training data
 
 # RF Model Testing
 
-rfPred.D <- predict.train(rfFit.D, testD[, !names(testD) %in% c("Type")], type = "raw")
+rfPred.D <- 
+  predict.train(rfFit.D, testD[, !names(testD) %in% c("Type")], type = "raw")
 
 toc() # Stop timing
 
 # Collect results in a list and save
 
-Mica.Sim.Prediction <- list(fit = rfFit.D,
-                               pred = predict.train(rfFit.D, testD[, !names(testD) %in% c("Type")], type = "raw"),
-                               confusion = confusionMatrix(rfPred.D, testD$Type),
-                               varImp = varImp(rfFit.D, scale = FALSE))
+Mica.Sim.Prediction <- 
+  list(fit = rfFit.D,
+  pred = predict.train(rfFit.D, testD[, !names(testD) %in% c("Type")], type = "raw"),
+  confusion = confusionMatrix(rfPred.D, testD$Type),
+  varImp = varImp(rfFit.D, scale = FALSE))
 
 sink(file = 'output/III_Drone2UNTRPred.txt')
 Mica.Sim.Prediction
@@ -359,10 +371,10 @@ sink()
 
 saveRDS(Mica.Sim.Prediction, 'output/III_Drone2UNTRPred.rds')
 
-########################################################################################
-# NOTES
+################################################################################
+#NOTES
 #
-# - Be careful with renaming columns for micasense bands! Check order!!
-# - Compare feature selection between classifications!
-# - How many bootstrap samples are recommended? As many as trees?
-# - Sample size between classifications?
+#- Be careful with renaming columns for micasense bands! Check order!! 
+#- Compare feature selection between classifications! 
+#- How many bootstrap samples are recommended? As many as trees? 
+#- Sample size between classifications?
